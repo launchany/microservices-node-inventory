325ec4efeee28efe67069cbc85471ba998e92096
James Higginbotham <james@launchany.com>
2016-04-11 14:28:42 -0500

    Adjusting configuration to use the environment variable within the MongoDB URL for Cloud 66 Docker deployment support

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   db: {
-      production: "mongodb://mongodb:27017/product_quantities",
+      production: "mongodb://"+process.env.MONGODB_ADDRESS+":27017/product_quantities",
       development: "mongodb://"+process.env.MONGODB_ADDRESS+":27017/product_quantities",
   }
 };
